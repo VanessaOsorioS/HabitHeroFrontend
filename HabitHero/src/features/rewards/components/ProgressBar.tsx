e02155a7373c58c07d546f46611109646a1b04f1
paula-cano <paula.14canob@gmail.com>
2025-10-30 15:08:48 -0500

    Style: agrega icono de la barra de progreso

--- HabitHero/src/features/rewards/components/ProgressBar.tsx
+++ HabitHero/src/features/rewards/components/ProgressBar.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Image } from "react-native";
 import { styles } from "../styles/ProgressBarStyles";
 
 export default function ProgressBar({ progress }: { progress: number }) {
   const pct = Math.min(Math.max(progress, 0), 1);
   return (
-    <View style={styles.container}>
-      <View style={[styles.fill, { width: `${pct * 100}%` }]} />
+    <View style={styles.wrapper}>
+      <View style={styles.container}>
+        <View style={[styles.fill, { width: `${pct * 100}%` }]} />
+      </View>
+      <Image
+      source={require("../../../assets/libro.png")}
+      style={styles.icon}
+      resizeMode="contain"/>
     </View>
   );
 }
--- HabitHero/src/features/rewards/styles/ProgressBarStyles.ts
+++ HabitHero/src/features/rewards/styles/ProgressBarStyles.ts
@@ -1,7 +1,13 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
+  wrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    margin: 10,
+  },
   container: {
+    flex: 1,
     height: 10,
     backgroundColor: "rgba(248, 212, 131, 1)",
     borderRadius: 10,
@@ -11,4 +17,11 @@ export const styles = StyleSheet.create({
     height: "100%",
     backgroundColor: "rgba(68, 171, 216, 1)",
   },
+  icon: {
+    position: "absolute",
+    marginLeft: -10,
+    width: 32,
+    height: 32,
+    zIndex: 5, 
+  }
 });

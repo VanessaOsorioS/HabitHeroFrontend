2b569e42890130efa920010324dd2bd9b98baf03
paula-cano <paula.14canob@gmail.com>
2025-11-28 01:12:28 -0500

    style: correcion fondo de arbol

--- HabitHero/src/app/index.tsx
+++ HabitHero/src/app/index.tsx
@@ -2,15 +2,20 @@ import React, { useState } from "react";
 import RewardPages from "../features/rewards/pages/RewardPages";
 import MainLayout from "../shared/layouts/MainLayout";
 import { MenuOption } from "../shared/menu/MainMenu";
+import MissionsPage from "../features/missions/pages/MissionsPage";
 
 export default function App() {
   const [currentPage, setCurrentPage] = useState<MenuOption>("rewards");
 
+  const pages: Record<MenuOption, React.ReactNode> = {
+    rewards: <RewardPages />,
+    missions: <MissionsPage />,
+    avatar: <></>
+  };
+
   return (
     <MainLayout onSelectMenuOption={setCurrentPage}>
-      <RewardPages/>
-      {/* cuando tengas Avatar */}
-      {/* {currentPage === "avatar" && <AvatarPage />} */}
+      {pages[currentPage]}
     </MainLayout>
   );
 }
